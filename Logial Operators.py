aa0f5caa96675a2b04867859ac4c765595bc7d0e
Santhosh <133015754+SandySV22@users.noreply.github.com>
2024-02-07 21:30:45 +0530

    Logial Operators In Python

---
+++ Logial Operators.py
@@ -0,0 +1,14 @@
+'''
+Logial Operators
+
+and ==>  if two condition is Ture,it gives o/p True
+or  ==>  if ine condition is True ,it gives o/p True
+not ==>  if True,o/p False . if False,o/p True
+
+'''
+s=int(input("Enter The 1st Value :"))
+v=int(input("Enter The 2nd Value :"))
+print(s>=1 and v<=10)
+print(s>=1 or v<=10)
+print(not(s>=1 and v<=10))
+
